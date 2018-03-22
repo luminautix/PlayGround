45bf89fbb7daf0a0518f158c561003ea643569f9
luminautix <luminautix@users.noreply.github.com>
2018-03-22 10:58:05 +0100

    Objectdateien gelöscht

---
+++ C_von_Anfang_an/bspl0004.c
@@ -0,0 +1,8 @@
+/* bspl0004.c */
+#include <stdio.h>
+main(){printf(
+"\nDies ist ein erstes C-Programm.\n");printf(
+"\nWie sie Sehen, kann 'printf' ");printf(
+"nicht nur Texte drucken.\n");printf(
+"sondern auch rechnen.\n\n");printf("13 * 7 = %i \n"
+,13 * 7);}
